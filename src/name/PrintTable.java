aa7b4aa0019eb7f7fe2258384a399876cfb0de0f
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2023-12-06 15:43:07 +0530

    Add files via upload

---
+++ src/name/PrintTable.java
@@ -0,0 +1,17 @@
+package programs;
+import java.util.*;
+
+public class PrintTable {
+	
+	public static void printTables(int number) {
+		for(int i=1; i<11; i++) {
+			System.out.print(number*i+" ");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		int number = new Scanner(System.in).nextInt();
+		printTables(number);
+	}
+}
