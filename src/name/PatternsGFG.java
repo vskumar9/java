4b8a25cf9d14a835d9bf04c2582e905b7f80d55b
V_SANJEEV_KUMAR <72122950+vskumar9@users.noreply.github.com>
2023-12-06 15:12:41 +0530

    Add files via upload

---
+++ src/name/PatternsGFG.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class PatternsGFG {
+	
+	public static void pattern(int number) {
+		for(int i = number; i>=1; i--) {
+			for(int j = number; j>0; j--) {
+				for(int k = i ; k>0; k--) {
+					System.out.print(j);
+				}
+//				break;
+			}
+			System.out.print(" $");
+		}
+	}
+	
+	public static void main(String[] args) {
+		pattern(3);
+	}
+}
